eb11ae1c2f6d59a7470c31777d4511dbd25aefa1
Arne Neumann <github@arne.cl>
2014-08-20 12:35:28 +0200

    added setup.py. had to rename sos to sos.py, but sos command is avail. after install

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+setup(name='sos',
+      version='0.1.0',
+      description='Stochastic Outlier Selection',
+      long_description=README,
+      author='Jeroen Janssens',
+      author_email='',
+      url='https://github.com/jeroenjanssens/sos',
+      license='',
+      package_dir={'sos': 'bin'},
+      packages=['sos'],
+      data_files=[('sos', ['bin/iris.csv'])],
+      install_requires=['numpy'],
+
+      include_package_data=True,
+    zip_safe=False,
+      entry_points={'console_scripts':
+          ['sos=sos:main']
+    }
+)
+
+
