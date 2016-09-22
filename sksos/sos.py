3cb89f2e52d3adecbae9afb132b6e4f17bc1e9ad
Jeroen Janssens <jeroen@jeroenjanssens.com>
2016-09-22 10:09:39 +0200

    Create class SOS

--- sksos/sos.py
+++ sksos/sos.py
@@ -1,23 +1,4 @@
 #!/usr/bin/env python
-#
-# Stochastic Outlier Selection
-# 
-# Copyright (c) 2013, Jeroen Janssens
-# All rights reserved.
-#
-# Distributed under the terms of the BSD Simplified License.
-# The full license is in the LICENSE file, distributed with this software.
-#
-# For more information about SOS, see https://github.com/jeroenjanssens/sos
-# J.H.M. Janssens, F. Huszar, E.O. Postma, and H.J. van den Herik. Stochastic
-# Outlier Selection. Technical Report TiCC TR 2012-001, Tilburg University,
-# Tilburg, the Netherlands, 2012.
-#
-# Please note that because SOS is inspired by t-SNE (created by Laurens 
-# van der Maaten; see http://homepage.tudelft.nl/19j49/t-SNE.html),
-# this code borrows functionality from the Python implementation,
-# namely the functions x2p and Hbeta.
-
 
 import argparse
 import logging
@@ -29,91 +10,136 @@ log_format = '%(asctime)-15s  [%(levelname)s] - %(name)s: %(message)s'
 logging.basicConfig(format=log_format, level=logging.INFO)
 log = logging.getLogger('SOS')
 
+class SOS(object):
 
-def x2d(X, metric):
-    """Computer dissimilarity matrix."""
+    """Stochastic Outlier Selection.
 
-    metric = metric.lower()
-    (n, d) = X.shape
-    log.debug("The data set is %dx%d", n, d)
-    if metric == 'none':
-        if n != d:
-            log.error(("If you specify 'none' as the metric, the data set "
-                "should be a square dissimilarity matrix"))
-            exit(1)
-        else:
-            log.debug("The data set is a dissimilarity matrix")
-            D = X
-    elif metric == 'euclidean':
-        log.debug("Computing dissimilarity matrix using Euclidean metric")
-        sumX = np.sum(np.square(X), 1)
-	# np.abs protects against extremely small negative values 
-	# that may arise due to floating point arithmetic errors
-        D = np.sqrt( np.abs(np.add(np.add(-2 * np.dot(X, X.T), sumX).T, sumX)) )
-    else:
-        try:
-            from scipy.spatial import distance
-        except ImportError as e:
-            log.error(("Please install scipy if you wish to use a metric "
-                "other than 'euclidean' or 'none'"))
-            exit(1)
-        else:
-            log.debug("Computing dissimilarity matrix using %s metric",
-                metric.capitalize())
-            D = distance.squareform(distance.pdist(X, metric))
-    return D
+    Copyright (c) 2013, Jeroen Janssens
+    All rights reserved.
 
+    Distributed under the terms of the BSD Simplified License.
+    The full license is in the LICENSE file, distributed with this software.
 
-def d2a(D, perplexity, tol=1e-5):
-    """Return affinity matrix.
+    For more information about SOS, see https://github.com/jeroenjanssens/sos
+    J.H.M. Janssens, F. Huszar, E.O. Postma, and H.J. van den Herik. Stochastic
+    Outlier Selection. Technical Report TiCC TR 2012-001, Tilburg University,
+    Tilburg, the Netherlands, 2012.
 
-    Performs a binary search to get affinities in such a way that each
-    conditional Gaussian has the same perplexity.
+    Please note that because SOS is inspired by t-SNE (created by Laurens
+    van der Maaten; see http://homepage.tudelft.nl/19j49/t-SNE.html),
+    this code borrows functionality from the Python implementation,
+    namely the functions x2p and Hbeta.
 
     """
 
-    (n, _) = D.shape
-    A = np.zeros((n, n))
-    beta = np.ones((n, 1))
-    logU = np.log(perplexity)
+    def __init__(self, perplexity=30, metric='euclidean', eps=1e-5):
+        self.perplexity = perplexity
+        self.metric = metric.lower()
+        self.eps = eps
 
-    for i in range(n):
-        if i % 100 == 0:
-            log.debug("Computing affinities (%d/%d)", i, n)
 
-        # Compute the Gaussian kernel and entropy for the current precision
-        betamin = -np.inf 
-        betamax =  np.inf
-        Di = D[i, np.concatenate((np.r_[0:i], np.r_[i+1:n]))]
-        (H, thisA) = get_perplexity(Di, beta[i])
+    def x2d(self, X):
+        """Computer dissimilarity matrix."""
 
-        # Evaluate whether the perplexity is within tolerance
-        Hdiff = H - logU
-        tries = 0
-        while np.abs(Hdiff) > tol and tries < 5000:
-            # If not, increase or decrease precision
-            if Hdiff > 0:
-                betamin = beta[i].copy()
-                if betamax == np.inf or betamax == -np.inf:
-                    beta[i] = beta[i] * 2.0
-                else:
-                    beta[i] = (beta[i] + betamax) / 2.0
+        (n, d) = X.shape
+        log.debug("The data set is %dx%d", n, d)
+        if self.metric == 'none':
+            if n != d:
+                log.error(("If you specify 'none' as the metric, the data set "
+                    "should be a square dissimilarity matrix"))
+                exit(1)
             else:
-                betamax = beta[i].copy()
-                if betamin == np.inf or betamin == -np.inf:
-                    beta[i] = beta[i] / 2.0
-                else:
-                    beta[i] = (beta[i] + betamin) / 2.0
-            # Recompute the values
+                log.debug("The data set is a dissimilarity matrix")
+                D = X
+        elif self.metric == 'euclidean':
+            log.debug("Computing dissimilarity matrix using Euclidean metric")
+            sumX = np.sum(np.square(X), 1)
+
+            # np.abs protects against extremely small negative values
+            # that may arise due to floating point arithmetic errors
+            D = np.sqrt( np.abs(np.add(np.add(-2 * np.dot(X, X.T), sumX).T, sumX)) )
+        else:
+            try:
+                from scipy.spatial import distance
+            except ImportError as e:
+                log.error(("Please install scipy if you wish to use a metric "
+                    "other than 'euclidean' or 'none'"))
+                exit(1)
+            else:
+                log.debug("Computing dissimilarity matrix using %s metric",
+                    self.metric.capitalize())
+                D = distance.squareform(distance.pdist(X, self.metric))
+        return D
+
+
+    def d2a(self, D):
+        """Return affinity matrix.
+
+        Performs a binary search to get affinities in such a way that each
+        conditional Gaussian has the same perplexity.
+
+        """
+
+        (n, _) = D.shape
+        A = np.zeros((n, n))
+        beta = np.ones((n, 1))
+        logU = np.log(self.perplexity)
+
+        for i in range(n):
+            if i % 100 == 0:
+                log.debug("Computing affinities (%d/%d)", i, n)
+
+            # Compute the Gaussian kernel and entropy for the current precision
+            betamin = -np.inf
+            betamax =  np.inf
+            Di = D[i, np.concatenate((np.r_[0:i], np.r_[i+1:n]))]
             (H, thisA) = get_perplexity(Di, beta[i])
+
+            # Evaluate whether the perplexity is within tolerance
             Hdiff = H - logU
-            tries += 1
+            tries = 0
+            while np.abs(Hdiff) > self.eps and tries < 5000:
+                # If not, increase or decrease precision
+                if Hdiff > 0:
+                    betamin = beta[i].copy()
+                    if betamax == np.inf or betamax == -np.inf:
+                        beta[i] = beta[i] * 2.0
+                    else:
+                        beta[i] = (beta[i] + betamax) / 2.0
+                else:
+                    betamax = beta[i].copy()
+                    if betamin == np.inf or betamin == -np.inf:
+                        beta[i] = beta[i] / 2.0
+                    else:
+                        beta[i] = (beta[i] + betamin) / 2.0
+                # Recompute the values
+                (H, thisA) = get_perplexity(Di, beta[i])
+                Hdiff = H - logU
+                tries += 1
 
-        # Set the final row of A
-        A[i, np.concatenate((np.r_[0:i], np.r_[i+1:n]))] = thisA
+            # Set the final row of A
+            A[i, np.concatenate((np.r_[0:i], np.r_[i+1:n]))] = thisA
 
-    log.debug("Computing affinities (%d/%d)", n, n)
-    return A
+        log.debug("Computing affinities (%d/%d)", n, n)
+        return A
+
+    def a2b(self, A):
+        B = A / A.sum(axis=1)[:,np.newaxis]
+        return B
+
+    def b2o(self, B):
+        O = np.prod(1-B, 0)
+        return O
+
+    def fit(self, X):
+        pass
+
+    def predict(self, X):
+        D = self.x2d(X)
+        A = self.d2a(D)
+        B = self.a2b(A)
+        O = self.b2o(B)
+        return O
 
 
 def get_perplexity(D, beta):
@@ -126,29 +152,6 @@ def get_perplexity(D, beta):
     sumA = sum(A)
     H = np.log(sumA) + beta * np.sum(D * A) / sumA
     return H, A
-
-
-def a2b(A):
-    log.debug("Computing binding probabilities")
-    B = A / A.sum(axis=1)[:,np.newaxis]
-    return B
-
-
-def b2o(B):
-    log.debug("Computing outlier probabilities")
-    O = np.prod(1-B, 0)
-    return O
-
-
-def sos(X, metric, perplexity, args):
-    D = x2d(X, metric)
-    A = d2a(D, perplexity)
-    B = a2b(A)
-    if args.binding_matrix:
-        np.savetxt(args.output, B, '%1.8f', delimiter=',')
-        exit()
-    O = b2o(B)
-    return O
 
 
 def main():
@@ -183,7 +186,9 @@ def main():
 
     log.debug("Reading data set from %s", args.input.name)
     X = np.loadtxt(args.input, delimiter=args.delimiter, ndmin=2)
-    O = sos(X, args.metric, args.perplexity, args)
+
+    O = SOS(args.perplexity, args.metric).predict(X)
+
     if args.threshold is None:
         log.debug("Writing outlier probabilities to %s", args.output.name)
         np.savetxt(args.output, O, '%1.8f')
